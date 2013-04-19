02653b5b9ffbcfb5aba83e88689d38dc4d09e654
ducluong <luongtranduc@gmail.com>
2013-04-20 04:35:06 +0700

    fix ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,7 +26,7 @@ $(function() {
 		
 		//$.post('/dashboard/view-options', $('#view_mode_frm').serialize());
 		//alert($('#view_mode_frm').serialize());
-		$.post('/dashboard/view-options', $('#option_view').serialize());
+		$.post('/dashboard/view-options', $('#view_mode_frm').serialize());
 	});
 });
 
