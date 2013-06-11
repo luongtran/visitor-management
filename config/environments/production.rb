2cffd5f38324df13ee414f790117c185c17bd0dc
Pavana Teja T <pt4189@gmail.com>
2013-06-11 21:35:01 +0530

    Added assets config to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,7 @@ VisitorManagement::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.default_url_options = { :host => '34pass.com', :protocol => 'http' }
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
@@ -15,7 +16,7 @@ VisitorManagement::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
