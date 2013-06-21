9849f70e903816c0ef6a71e7b12c58de6b960a5c
Andrey Lukashchuk <darksundarksun@gmail.com>
2013-06-21 19:32:27 +0300

    changed mailing adresses

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,10 +1,9 @@
 class AdminMailer < ActionMailer::Base
   include Devise::Mailers::Helpers
   default from: "contact@16columns.com",
-          to:   ["darksundarksun@gmail.com", "darksun8@yandex.ru"]
+          to:   ["vishant.pai@gmail.com", "vishant.pai@icloud.com"]
 
   def user_registered_message(user)
-  	#emails = ["vishant.pai@gmail.com", "vishant.pai@icloud.com"]
   	@user = user
     subject = "34pass.com - User Registered"
     mail(:subject => subject)
