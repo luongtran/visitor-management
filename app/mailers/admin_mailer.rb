01afde8dacba9fc01685c63f93e04a05e478ecbc
Andrey Lukashchuk <darksundarksun@gmail.com>
2013-06-21 19:15:41 +0300

    fixed email

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,12 +1,12 @@
 class AdminMailer < ActionMailer::Base
   include Devise::Mailers::Helpers
   default from: "contact@16columns.com"
+          to:   ["darksundarksun@gmail.com", "darksun8@yandex.ru"]
 
   def user_registered_message(user)
+  	#emails = ["vishant.pai@gmail.com", "vishant.pai@icloud.com"]
   	@user = user
     subject = "34pass.com - User Registered"
-    mail(:to => "vishant.pai@gmail.com",  :subject => subject)
-    mail(:to => "vishant.pai@icloud.com", :subject => subject)
-
+    mail(:subject => subject)
   end
 end
