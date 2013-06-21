c679de931e0e2e744407ad6de8ec0877555c07f5
Andrey Lukashchuk <darksundarksun@gmail.com>
2013-06-21 19:23:45 +0300

    fixed email

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,6 +1,6 @@
 class AdminMailer < ActionMailer::Base
   include Devise::Mailers::Helpers
-  default from: "contact@16columns.com"
+  default from: "contact@16columns.com",
           to:   ["darksundarksun@gmail.com", "darksun8@yandex.ru"]
 
   def user_registered_message(user)
