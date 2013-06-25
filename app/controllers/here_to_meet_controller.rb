dcda1b65914f71c62843e156ced4356b17dc5ef9
Andrey Lukashchuk <darksundarksun@gmail.com>
2013-06-25 12:03:37 +0300

    fixed here_to_meet

--- app/controllers/here_to_meet_controller.rb
+++ app/controllers/here_to_meet_controller.rb
@@ -1,8 +1,8 @@
 class HereToMeetController < ApplicationController
 
 	def show
-		#@list = HereToMeet.find(params[:id], :limit => 5)
-		@list = HereToMeet.where('organization = ? AND badge_nubmer LIKE ?',
+		#@list = HereToMeet.find(params[:id]f, :limit => 5)
+		@list = HereToMeet.where('organization = ? AND name LIKE ?',
 			    current_user.organisation_name, '%' + params[:id] + '%').limit(5)
 
 		respond_to do |format|
