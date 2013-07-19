577f1cfd5b09e5971e973e2d8fed97cbf02ff968
Akram Khan <akramkhan83@gmail.com>
2013-07-20 04:02:41 +0530

    added checknet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,42 +1,11 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
-// $(document).ready(function(){
-// 
-//   $(".search_dropdown").change(function(e) {
-//     if($(this).val() == "now_inside") {
-//       debugger;
-//       $.ajax({
-//         type: "POST",
-//         url: "/visitors/search",
-//         data: {search_keyword: "now_inside"},
-//         dataType: JSON,
-//         success: function (data) {
-//           debugger;
-//         },
-//         error: function(e) {
-//           console.log(e);
-//         }
-//       
-//       
-//       });
-//     }
-//   })
-//     
-// })
+
+$(document).ready(function(){
+    $.fn.checknet();
+});
 
 $(function() {
 	change_name_selectbox();
@@ -55,11 +24,6 @@ $(function() {
 		
 		$('form#view_mode_frm').trigger('submit.rails');
 		
-		//$.post('/dashboard/view-options', $('#view_mode_frm').serialize());
-		//alert($('#view_mode_frm').serialize());
-		// $.post('/dashboard/view-options', $('#view_mode_frm').serialize(), function(data) {
-			// $('#dashboard_content').html(data);
-		// });
 	});
   
 	
